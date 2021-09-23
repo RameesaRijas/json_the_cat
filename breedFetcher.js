43a543698a9544678e7989dc46560340cadd119b
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-23 14:30:48 -0400

    test and refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require("request");
-const args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(error.message);
-    return;
-  }
-  if (body.length) {
-    console.log("Breed Not Found");
-  } else {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error.message);
+      return;
+    }
     const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+    if (!data.length) {
+      callback("Breed Not Found", null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
